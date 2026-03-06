6c2859565a8b043e9c57982ebaca78bf030f0598
Kaissa27 <86300276+Kaissa27@users.noreply.github.com>
2026-03-07 00:02:05 +0530

    Updated

--- Bank Account Simulator.py
+++ Bank Account Simulator.py
@@ -17,7 +17,7 @@ your_car.display_info()
 class Dog:
   def __init__(self, name, breed):
     self.name = name
-    self.breed = breed
+    self.breed = breed 
 
   def bark(self):
     print(f"{self.name} is barking!")
