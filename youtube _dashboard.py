9c824df7c8d46b91f2304ae8630b6be057cfca1e
Sania <86300276+Kaissa27@users.noreply.github.com>
2026-09-23 22:06:40 +0530

    Update youtube _dashboard.py

--- youtube _dashboard.py
+++ youtube _dashboard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(page_title="YouTube Analytics", layout="wide")
-st.title("📺 YouTube Channel Analytics Dashboard")
+st.title(" YouTube Channel Analytics Dashboard")
 
 @st.cache_data
 def load_data():
